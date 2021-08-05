f8a460e49f0ed1785e680ed5bde25fb74d7810e2
jamesie <fenb1303@gmail.com>
2021-08-05 22:53:12 +0930

    added string highlighter(self-explanitory), added a response line function to cut down on repetitive code and finally changed compute function to allow new response system aswell as browser alerting for incorrect for inputs

--- script.js
+++ script.js
@@ -1,15 +1,38 @@
 function compute()
 {
     var x = document.getElementById("principal").value;
-    if (!x) return
+    if (!x || x < 1){
+        alert(`Error: you must enter a positive number for amount!`)
+        document.getElementById("principal").focus();
+        return
+    } 
+    document.getElementById("res").innerHTML = null
     var y = document.getElementById("rate").value;
     var z = document.getElementById("years").value;
     const calc = (x * y * z) / 100
     var a = new Date()
     const date = a.getFullYear() + parseInt(z);
+    const response = [createResponseLine("If you deposit $", ",", x) , createResponseLine("at an interest rate of ", "%.", y), createResponseLine("You will recieve an amount of ", ",", calc), createResponseLine("in the year ", ".", date)]
+    response.forEach((element) => {
+        document.getElementById("res").appendChild(element)
+    })
+}
 
-    document.getElementById("res").innerHTML = `If you deposit $${x}, \nat an interest rate of ${y}%. \nYou will recieve an amount of ${calc},\nin the year ${date}`
-    
+function stringHighlighter(text){
+    const element  = document.createElement("mark")
+    element.innerHTML = text
+    return element
+}
+
+function createResponseLine(text, text2, highlight,){
+    const element = document.createElement("div")
+    const textnode = document.createTextNode(text);
+    const text2node = document.createTextNode(text2);
+    element.appendChild(textnode)
+    element.appendChild(stringHighlighter(highlight))
+    element.appendChild(text2node)
+    element.setAttribute("class", "flex")
+    return element
 }
 
 
